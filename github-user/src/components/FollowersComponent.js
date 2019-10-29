c08e14d8fec9370985779209e12574b307d95b96
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-29 15:34:13 -0700

    styling followers card

--- github-user/src/components/FollowersComponent.js
+++ github-user/src/components/FollowersComponent.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const FollowerContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-content: center; 
+max-width: 50%;
+margin 0 auto; 
+text-align: center;
+`
 
 const Followers = props => {
     return (
-        <div key = {props.follower.id}>
-            <h2>{props.follower.name}</h2>
-            <p>{props.follower.bio}</p>
+        <FollowerContainer key = {props.follower.id}>
+            <img src ={props.follower.avatar_url}/>
+            <h2>{props.follower.login}</h2>
+            <p>Pofile: <a href= {props.follower.html_url} target='_blank'>{props.follower.html_url}</a></p>
             <p>{props.follower.location}</p>
 
-        </div>
+        </FollowerContainer>
     )
 }
 
