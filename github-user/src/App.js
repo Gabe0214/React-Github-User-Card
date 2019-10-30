a426c006909cb8c081b6ddda2b3ca4db1f7c2a0e
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-29 17:09:53 -0700

    created fetchFollowers function and invoked it in the componentDidMount to display followeres

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import './App.css';
-
+import axios from 'axios'
+import UserCard from './components/UserCard'
+import FollowersComponent from './components/FollowersComponent'
  class App extends React.Component {
   constructor(){
     super()
-    this.state = { name: 'Gabe'}
+    this.state = { 
+      user: {}, 
+      followers: [] 
+    }
   }
   
-  
+  componentDidMount() {
+    axios 
+     .get('https://api.github.com/users/Gabe0214') 
+       .then(res => {
+         console.log(res.data)
+         this.setState({
+           user: res.data
+         })
+       }, )
+     this.fetchFollowers(); 
+  }
 
+  fetchFollowers = () => {
+    axios
+    .get('https://api.github.com/users/Gabe0214/followers')
+     .then(res => {
+       console.log(res)
+       this.setState({
+         followers: res.data
+       })
+        
+       
+     })
+     .catch(error => {
+       console.log('error', error);
+     })
+     
+  }
+   
    
    render(){  
     return (
     <div>
-      <h1>{this.state.name}</h1>
+      <UserCard user ={this.state.user} src ={this.state.user}/> 
+       {this.state.followers.map(follower => (
+         <FollowersComponent follower ={follower}/> 
+       ))}
     </div>
     );
     } 
