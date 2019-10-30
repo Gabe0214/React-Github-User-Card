037b7d814a38dbc8e4b3d2877d3572a0539bca7c
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-29 17:10:33 -0700

    completed styling for both followers and user cards

--- github-user/src/components/FollowersComponent.js
+++ github-user/src/components/FollowersComponent.js
@@ -6,20 +6,34 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-content: center; 
-max-width: 50%;
-margin 0 auto; 
+max-width: 30%;
+margin:  0 auto; 
 text-align: center;
+border-radius: 5px;
+  box-shadow: 0 1px 6px -2px #000;
+  background-color: #FFF;
+  width: 100%; 
+  margin-bottom: 2rem;
 `
+ const Img = styled.img` 
+ 
+ width: 30%;
+ margin-top: 0.8rem;
+ align-self: center;
+
+`
+
 
 const Followers = props => {
     return (
+        
         <FollowerContainer key = {props.follower.id}>
-            <img src ={props.follower.avatar_url}/>
+            <Img src ={props.follower.avatar_url}/>
             <h2>{props.follower.login}</h2>
-            <p>Pofile: <a href= {props.follower.html_url} target='_blank'>{props.follower.html_url}</a></p>
-            <p>{props.follower.location}</p>
-
+            <p>Pofile: <a href= {props.follower.html_url} target='_blank'>{props.follower.html_url}</a> </p> 
+        
         </FollowerContainer>
+        
     )
 }
 
--- github-user/src/components/UserCard.js
+++ github-user/src/components/UserCard.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Img = styled.img`
+width: 30%;
+margin-top: 0.8rem;
+align-self: center;
+
+`
+
+const UserContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-content: center; 
+max-width: 30%;
+margin:  0 auto; 
+text-align: center;
+border-radius: 5px;
+  box-shadow: 0 1px 6px -2px #000;
+  background-color: #FFF;
+  width: 100%; 
+  margin-bottom: 2rem;
+
+`
 const User = props => {
   return(
-      <div>
-          <img src ={props.user.img}/>
+      <UserContainer>
+          <Img src ={props.user.avatar_url}/>
           <h2>{props.user.name}</h2>
-          <p>{props.user.bio}</p>
-          <p>{props.user.follwers}</p>
-      </div>
+          <p> Bio : {props.user.bio}</p>
+          <p>Followers: {props.user.followers}</p>
+      </UserContainer>
   )
 }
 
